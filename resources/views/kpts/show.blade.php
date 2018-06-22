03d94dc71c1a67ad746511088a90f9b157d37fcc
eriko.kashimura <eriko.kashimura@rakuten.com>
2018-06-22 05:41:37 +0000

    show kaetayo

--- resources/views/kpts/show.blade.php
+++ resources/views/kpts/show.blade.php
@@ -1,45 +1,88 @@
+<style>
+.contents {
+    font-family: 'Monotype Corsiva';
+    font-size: 50px;
+    text-shadow: 3px 3px 3px #79EAE6;
+    }
+    
+.box16{
+    padding: 0.5em 1em;
+    margin: 2em 0;
+    background: -webkit-repeating-linear-gradient(-45deg, #f0f8ff, #f0f8ff 3px,#e9f4ff 3px, #e9f4ff 7px);
+    background: repeating-linear-gradient(-45deg, #f0f8ff, #f0f8ff 3px,#e9f4ff 3px, #e9f4ff 7px);
+}
+.box16 p {
+    margin: 0; 
+    padding: 0;
+}
+
+.panel{
+    overflow-y:scroll;
+}
+    
+
+</style>
+
 @extends('layouts.app')
 
 @section('content')
     
     
-    <h1>{{ $kpt->dates }} の詳細ページ</h1>
-    
-    <div class="table-responsive">
-    <table class="table table-striped table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th>Dates</th>
-                            <th>Contents</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                            <tr>
-                                <td><b>{{ $kpt->dates }}</b></td>
-                                <td>
-                                    <p><b>Keep:</b><br> {!! nl2br($kpt->keep) !!}</p>
-                                    <p><b>Problem:</b><br> {!! nl2br($kpt->problem) !!}</p>
-                                    <p><b>Try:</b><br> {!! nl2br($kpt->Try) !!}</p>
-                                    <p><b>MVP:</b><br> {!! nl2br($kpt->MVP) !!}</p>
-                                    <p><b>Reason:</b><br> {!! nl2br($kpt->reason) !!}</p>
-                                    <p><b>Want:</b><br> {!! nl2br($kpt->want) !!}</p>
-                           
-                                
-                                </td>
-                            </tr>
-                    </tbody>
-            </table>
-    </div>
+
+<h1 class='contents'>{{ $kpt->dates }}&nbsp;&nbsp;&nbsp; Contents&nbsp;</h1>
+           
+        <div class='box16'>
+            
+            <h3><b>Keep</b></h3>
+            <div class='panel panel-info' style="height: 20vh;">
+                <h4>{!! nl2br($kpt->keep) !!}</h4>
+            </div>
+            
+            <h3><b>Problem</b></h3>
+            <div class='panel panel-info' style="height: 20vh;">
+                <h4>{!! nl2br($kpt->problem) !!}</h4>
+            </div>
+            
+            <h3><b>Try</b></h3>
+            <div class='panel panel-info' style="height: 20vh;">
+                <h4>{!! nl2br($kpt->try) !!}</h4>
+            </div>
+            
+            <h3><b>MVP</b></h3>
+            <div class='panel panel-info' style="height: 20vh;">
+                <h4>{!! nl2br($kpt->MVP) !!}</h4>
+            </div>
+            
+            <h3><b>Reason</b></h3>
+            <div class='panel panel-info' style="height: 20vh;">
+                <h4>{!! nl2br($kpt->reason) !!}</h4>
+            </div>
+            
+            <h3><b>Want</b></h3>
+            <div class='panel panel-info' style="height: 20vh;">
+                <h4>{!! nl2br($kpt->want) !!}</h4>
+            </div>
+        </div>
 <div class='btn-toolbar'>
     
     <!--先にゴミ箱を右に寄せる-->
 
         {!! Form::model($kpt, ['route' => ['kpts.destroy', $kpt->id], 'method' => 'delete'])!!}
-            {{ Form::button('<span class="glyphicon glyphicon-trash"></span>', array('class'=>'btn btn-danger pull-right', 'type'=>'submit')) }}
+            {{ Form::button('<span class="glyphicon glyphicon-trash"></span>', array('class'=>'btn btn-default btn-lg pull-right', 'type'=>'submit')) }}
         {!! Form::close() !!}
     
-     
-        {!! link_to_route('kpts.edit', ' このKPTを編集', ['id' => $kpt->id], ['class' => 'btn btn-primary glyphicon glyphicon-edit']) !!}
+        {!! link_to_route('kpts.edit', ' Edit', ['id' => $kpt->id], ['class' => 'btn btn-info btn-lg glyphicon glyphicon-apple']) !!}
+</div>
+<!--コピペ用-->
+<br>
+<h3>Copy&Paste</h3>
+<div class='panel panel-info'>
+        <h3><b>Keep</b></h3><p> {!! nl2br($kpt->keep) !!}</p>
+        <h3><b>Problem</b></h3><p> {!! nl2br($kpt->problem) !!}</p>
+        <h3><b>Try</b></h3><p>{!! nl2br($kpt->Try) !!}</p>
+        <h3><b>MVP</b></h3><p>{!! nl2br($kpt->MVP) !!}</p>
+        <h3><b>Reason</b></h3><p>{!! nl2br($kpt->reason) !!}</p>
+        <h3><b>Want</b></h3><p>{!! nl2br($kpt->want) !!}</p>
 </div>
 
     
